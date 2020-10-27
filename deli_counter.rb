3dffff831df1979b6fb10edf19aa2c7183f841b7
Joel-Wiersma-Flatiron <Joel.Wiersma.D@gmail.com>
2020-10-27 17:21:24 -0600

    commit tree 8bda79d7bb56584fa4e42fdc76880d5c8d9d4a3c

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+def line(people)
+    if people.size == 0
+        puts "The line is currently empty."
+    else
+        count = 1
+        line = "The line is currently:"
+        people.each do |person|
+            temp = " #{count}. #{person}"
+            line += temp
+            count += 1
+        end
+        puts line
+    end
+end
+
+def take_a_number(store, name)
+    store.push(name)
+    puts "Welcome, #{name}. You are number #{store.size} in line."
+end
+
+def now_serving(store)
+    if store.size == 0
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{store.shift}."
+    end
+end
